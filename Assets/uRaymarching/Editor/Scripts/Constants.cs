61f0077e00c92d8f26e1a7408b8146cd72eb58fe
tunchasan <hasantunc.dev@gmail.com>
2021-04-09 23:10:05 +0300

    Import uRaymarching v2.1.1

---
+++ Assets/uRaymarching/Editor/Scripts/Constants.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Raymarching
+{
+
+[CreateAssetMenu(
+    menuName = "Shader/uRaymarching/Constants", 
+    order = uShaderTemplate.Common.Setting.menuOrder + 1)]
+public class Constants : uShaderTemplate.Constants
+{
+	const string ShaderDir = "RaymarchingShaderDirectory";
+
+    [SerializeField]
+    string shaderName = "Hidden/Raymarching/UniversalRP/GetPathFromScript";
+
+	void Awake()
+	{
+		values = new uShaderTemplate.Constant[] {
+			new uShaderTemplate.Constant() { 
+				name = ShaderDir,
+				value = Utils.GetShaderDirPath(shaderName)
+			}
+		};
+	}
+
+	public override void OnBeforeConvert()
+	{
+		for (int i = 0; i < values.Length; ++i)
+		{
+			if (values[i].name != ShaderDir) continue;
+
+			var constant = values[i];
+			constant.value = Utils.GetShaderDirPath(shaderName);
+			values[i] = constant;
+		}
+	}
+}
+
+}
---
+++ Assets/uRaymarching/Editor/Scripts/Utils.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+namespace Raymarching
+{
+
+public static class Utils
+{
+    public static string GetShaderDirPath(string shaderName)
+    {
+        var shader = Shader.Find(shaderName);
+        var shaderPath = AssetDatabase.GetAssetPath(shader);
+        try
+        {
+            return Path.GetDirectoryName(shaderPath);
+        }
+        catch (System.Exception)
+        {
+            return "";
+        }
+    }
+}
+
+}
