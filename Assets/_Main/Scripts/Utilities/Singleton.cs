e8f14bca070b06968f605868e714617565195e95
tunchasan <hasantunc.dev@gmail.com>
2021-04-09 22:39:00 +0300

    Create Project v2020.2.2f1

---
+++ Assets/_Main/Scripts/Utilities/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static object _lock = new object();
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            lock (_lock)
+            {
+                if (_instance == null) _instance = (T) FindObjectOfType(typeof(T));
+
+                return _instance;
+            }
+        }
+    }
+}
